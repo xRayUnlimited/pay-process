f87301b825522928e0470d7aaa62a80e4e36cde0
Ray Suyoto <ray.suy@icloud.com>
2018-04-17 11:59:03 -0600

    success screen

--- client/src/components/PaymentSuccess.js
+++ client/src/components/PaymentSuccess.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Segment, Header } from 'semantic-ui-react';
+import { Link, Redirect } from 'react-router-dom';
 
-const PaymentSuccess = () => (
-  <div>Yay!</div>
+const PaymentSuccess = ({ location: { state }}) => {
+  if (state && state.transactionId)
+    return (
+      <Segment basic textAlign="center">
+        <Header
+          as="h1"
+          color="green"
+        >
+          Thank you for shopping
+        </Header>
+        <p>You have been charged ${state.amount}</p>
+        <p>Your Transaction ID is: {state.transactionId}</p>
+        <Link to="/">Buy more stuff</Link>
+      </Segment>
+    )
+  else
+    return <Redirect to="/" />
+}
 
-)
 export default PaymentSuccess;
